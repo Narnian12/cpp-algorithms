40fcbb524161a805975934a7b306d1b4fa0cd35d
Peter Sun <pitaah@umich.edu>
2021-10-28 10:15:32 -0400

    - Implemented working version of quicksort

---
+++ quicksort/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void swap(int *a, int *b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+// Use rightmost index as pivot
+void QuickSort(vector<int> &unsorted, int low, int pivot) {
+  if (pivot - low == 0) return;
+  if (pivot - low == 1) {
+    if (unsorted[low] > unsorted[pivot]) swap(&unsorted[low], &unsorted[pivot]);
+    return;
+  }
+  int newPivot = -1;
+  int swapIdx = -1;
+  for (int i = low; i < pivot; ++i) {
+    if (unsorted[i] < unsorted[pivot]) {
+      swapIdx = i;
+      // Swap the lower value to the left until it is further left than all values greater than pivot
+      while (swapIdx - 1 >= 0 && unsorted[swapIdx - 1] > unsorted[pivot]) {
+        swap(&unsorted[swapIdx - 1], &unsorted[swapIdx]);
+        newPivot = swapIdx;
+        --swapIdx;
+      }
+    }
+  }
+  // swapIdx is the rightmost edge of values lower than pivot, so set the newPivot to the leftmost edge of values greater than pivot
+  newPivot = swapIdx + 1;
+  swap(&unsorted[newPivot], &unsorted[pivot]);
+  if (pivot - low > 2) {
+    QuickSort(unsorted, 0, newPivot - 1);
+    QuickSort(unsorted, newPivot + 1, unsorted.size() - 1);
+  }
+}
+
+int main() {
+  vector<int> vect { 50, 23, 9, 18, 61, 10, 32 };
+  QuickSort(vect, 0, vect.size() -1 );
+  for (auto x : vect) {
+    cout << x << " ";
+  }
+}
